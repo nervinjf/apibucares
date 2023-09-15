530d4f28e6e344effa6e8eb0880df822a251f485
Nervin <nervinjflores@gmail.com>
2023-09-15 17:29:50 -0400

    se monta primera parte de la api

--- src/server.js
+++ src/server.js
@@ -9,5 +9,6 @@ const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`servidor corriendo en el puerto ${PORT}`);
+    console.log(`servidor corriendo en el puerto ${PORT}`);
 });
   

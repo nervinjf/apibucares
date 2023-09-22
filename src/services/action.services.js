84784ebe7298adb98f6c92b1ff47f9c4fe7ced38
nervinjf <109636023+nervinjf@users.noreply.github.com>
2023-09-22 17:11:59 -0400

    Update action.services.js

--- src/services/action.services.js
+++ src/services/action.services.js
@@ -106,14 +106,12 @@ class ActionServices {
                   console.log('Correo enviado a:', 'correo_destino@ejemplo.com');
                     console.log(`numero: ${Number(recipen.length)}, status: ${status}, total: ${totalpagar}`)
                     console.log(`where: { id: ${e.id} }`)
-                try{
-                  const update = await Vivienda.update({ recibospendientes: Number(recipen.length),  status: status }, {
-                    where: { id: e.id },
-                } )
-                }catch (error) {
-                console.log(error)
+                 const update = await Vivienda.update({ recibospendientes: Number(recipen.length),  status: status }, {
+                        where: { id: e.id },
+                      });
+
+                          console.log(update);
         }
-            }
 
         } catch (error) {
             throw error;

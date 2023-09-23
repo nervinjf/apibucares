8b3adadf72d8802825058cbde2d7048a6bf30bf5
nervinjf <109636023+nervinjf@users.noreply.github.com>
2023-09-23 16:07:28 -0400

    Update Recibo.js

--- src/templates/Recibo.js
+++ src/templates/Recibo.js
@@ -17,12 +17,14 @@ const recibocondominio = (data) => {
 
     });
 
+    console.log(data);
+
 const contenidoHTML = data?.uservivienda?.viviendaRecibo?.reciboRecibomodelo?.recibomodeloGastos?.map((e) =>
     `<div key=${e.id} style="display: flex; justify-content: center; align-items: center; border-top: 0.1rem solid black; height: 100%;">
         <p>${e.nombre}</p>
     </div>`).join('');
 
-    console.log(data)
+    console.log(contenidoHTML)
 
 const contenidoHTML1 = data?.uservivienda?.viviendaRecibo?.reciboRecibomodelo?.recibomodeloGastos?.map((e) =>
     `<div key=${e.id} style=" display: flex;

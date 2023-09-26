d5f898730ad0c5a43ec62b428933ef3b97f7b7cb
Nervin <nervinjflores@gmail.com>
2023-09-26 16:38:29 -0400

    Se acomoda el models de recibo

--- src/services/action.services.js
+++ src/services/action.services.js
@@ -182,7 +182,8 @@ align-items: center;
 border-top: 0.1rem solid black;
 height: 1rem;
 font-size: 0.8rem">
-    <p>Bs. ${new Intl.NumberFormat('es-VE', {  style: 'currency',
+    <p>Bs. ${new Intl.NumberFormat('es-VE', {
+        style: 'currency',
     currency: 'VES',
     thousandSeparator: '.',
     decimalSeparator: ',',

4419a74c476f669efa449fa75a4fa4d009af27e8
Nervin <nervinjflores@gmail.com>
2023-09-26 15:33:39 -0400

    Se acomoda el models de recibo

--- src/services/action.services.js
+++ src/services/action.services.js
@@ -182,7 +182,10 @@ align-items: center;
 border-top: 0.1rem solid black;
 height: 1rem;
 font-size: 0.8rem">
-    <p>Bs. ${(numeral(e?.monto * recibo?.reciboRecibomodelo?.bcv).format('Bs. 0,0.00'))}</p>
+    <p>Bs. ${(numeral(e?.monto * recibo?.reciboRecibomodelo?.bcv).format('Bs. 0,0.00', {
+        thousandSeparator: '.',
+        decimalSeparator: ','
+      }))}</p>
 </div>`).join('');
 
                 const contenidoHTML4 = recibo?.reciboRecibomodelo?.recibomodeloGastos?.map((e) =>

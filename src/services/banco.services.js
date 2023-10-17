90e93d15f9f2ea486ccba9d04e817442dd3f13ac
Nervin Flores <nervinjflores@gmail.com>
2023-10-17 16:54:51 -0400

    Se agrega el api del banco

--- src/services/banco.services.js
+++ src/services/banco.services.js
@@ -1,66 +1,64 @@
 const { Abastecimiento } = require('../models');
 const axios = require('axios');
-const oauth = require('axios-oauth-client')
-const tokenProvider = require("axios-token-interceptor");
-require("dotenv").config();
+const oauth = require('axios-oauth-client');
+const tokenProvider = require('axios-token-interceptor');
+require('dotenv').config();
 
 class BancoServices {
-
-    static async get() {
-        try {
-            const result = await Abastecimiento.findAll()
-            return result;
-        } catch (error) {
-            throw error;
-        }
+  static async get() {
+    try {
+      const result = await Abastecimiento.findAll();
+      return result;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async postPagobdv(data) {
-        try {
+  static async postPagobdv(data) {
+    try {
+      const options = {
+        headers: {
+          'Content-Type': 'application/json',
+          accept: 'application/json',
+        },
+      };
 
-            const options = {};
-            options.headers = {
-                "Content-Type": "application/json",
-                accept: "application/json"
-            };
-            const client = axios.create(options);
+      const client = axios.create(options);
 
-            const getOwnerCredentials = oauth.client(axios.create(), {
-                url: "https://biodemo.ex-cle.com:4443/Biopago2/IPG2/oauth2/token",
-                grant_type: "client_credentials",
-                client_id: process.env.CLIENT_ID,
-                client_secret: process.env.CLIENT_SECRET,
-            });
+      const getOwnerCredentials = oauth.client(client, {
+        url: 'https://biodemo.ex-cle.com:4443/Biopago2/IPG2/oauth2/token',
+        grant_type: 'client_credentials',
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+      });
 
-            client.interceptors.request.use(
-                oauth.interceptor(tokenProvider, getOwnerCredentials)
-            );
+      client.interceptors.request.use(oauth.interceptor(tokenProvider, getOwnerCredentials));
 
-            const { amount, number, casa, fecha, cellPhone, email, urlToReturn } = data
+      const { amount, number, casa, fecha, cellPhone, email, urlToReturn } = data;
 
-            const data2 = {
-                "currency": 1,
-                "amount": amount,
-                "reference": `PRB${casa}-${fecha.replace('-', "").replace('-', "")}`,
-                "title": "Pago factura",
-                "description": "Cancela condominio",
-                "letter": "V",
-                "number": number,
-                "urlToReturn": urlToReturn,
-                "cellPhone": cellPhone,
-                "email": email
-            }
+      const paymentData = {
+        currency: 1,
+        amount: amount,
+        reference: `PRB${casa}-${fecha.replace('-', '').replace('-', '')}`,
+        title: 'Pago factura',
+        description: 'Cancela condominio',
+        letter: 'V',
+        number: number,
+        urlToReturn: urlToReturn,
+        cellPhone: cellPhone,
+        email: email,
+      };
 
-            // Realiza la solicitud a la API
-            const result = await client.post('https://biodemo.ex-cle.com:4443/Biopago2/IPG2/api/Payments', data2)
-            console.log(result)
-            return result;
-        } catch (error) {
-            console.log(error)
-            throw error;
-        }
+      // Realiza la solicitud a la API
+      const response = await client.post('https://biodemo.ex-cle.com:4443/Biopago2/IPG2/api/Payments', paymentData);
+      
+      console.log(response.data); // Puedes acceder a los datos de la respuesta usando response.data
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-
+  }
 }
 
 module.exports = BancoServices;

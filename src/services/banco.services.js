d0d7f640155e56c6f73271294b049974137d318e
Nervin Flores <nervinjflores@gmail.com>
2023-10-17 17:00:08 -0400

    Se agrega el api del banco

--- src/services/banco.services.js
+++ src/services/banco.services.js
@@ -5,60 +5,60 @@ const tokenProvider = require('axios-token-interceptor');
 require('dotenv').config();
 
 class BancoServices {
-  static async get() {
-    try {
-      const result = await Abastecimiento.findAll();
-      return result;
-    } catch (error) {
-      throw error;
+    static async get() {
+        try {
+            const result = await Abastecimiento.findAll();
+            return result;
+        } catch (error) {
+            throw error;
+        }
     }
-  }
 
-  static async postPagobdv(data) {
-    try {
-      const options = {
-        headers: {
-          'Content-Type': 'application/json',
-          accept: 'application/json',
-        },
-      };
+    static async postPagobdv(data) {
+        try {
+            // Obtener el token de OAuth2 usando el flujo de credenciales del cliente
+            const tokenResponse = await axios.post('https://biodemo.ex-cle.com:4443/Biopago2/IPG2/oauth2/token', {
+                grant_type: 'client_credentials',
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
+            });
 
-      const client = axios.create(options);
+            const accessToken = tokenResponse.data.access_token;
 
-      const getOwnerCredentials = oauth.client(client, {
-        url: 'https://biodemo.ex-cle.com:4443/Biopago2/IPG2/oauth2/token',
-        grant_type: 'client_credentials',
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-      });
+            // Configurar las cabeceras con el token de acceso
+            const headers = {
+                'Content-Type': 'application/json',
+                accept: 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            };
 
-      client.interceptors.request.use(oauth.interceptor(tokenProvider, getOwnerCredentials));
+            const { amount, number, casa, fecha, cellPhone, email, urlToReturn } = data;
 
-      const { amount, number, casa, fecha, cellPhone, email, urlToReturn } = data;
+            const paymentData = {
+                currency: 1,
+                amount: amount,
+                reference: `PRB${casa}-${fecha.replace('-', '').replace('-', '')}`,
+                title: 'Pago factura',
+                description: 'Cancela condominio',
+                letter: 'V',
+                number: number,
+                urlToReturn: urlToReturn,
+                cellPhone: cellPhone,
+                email: email,
+            };
 
-      const paymentData = {
-        currency: 1,
-        amount: amount,
-        reference: `PRB${casa}-${fecha.replace('-', '').replace('-', '')}`,
-        title: 'Pago factura',
-        description: 'Cancela condominio',
-        letter: 'V',
-        number: number,
-        urlToReturn: urlToReturn,
-        cellPhone: cellPhone,
-        email: email,
-      };
+            // Realiza la solicitud a la API con el token de acceso
+            const response = await axios.post('https://biodemo.ex-cle.com:4443/Biopago2/IPG2/api/Payments', paymentData, {
+                headers: headers,
+            });
 
-      // Realiza la solicitud a la API
-      const response = await client.post('https://biodemo.ex-cle.com:4443/Biopago2/IPG2/api/Payments', paymentData);
-      
-      console.log(response.data); // Puedes acceder a los datos de la respuesta usando response.data
-      return response.data;
-    } catch (error) {
-      console.error(error);
-      throw error;
+            console.log(response.data); // Puedes acceder a los datos de la respuesta usando response.data
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     }
-  }
 }
 
 module.exports = BancoServices;
